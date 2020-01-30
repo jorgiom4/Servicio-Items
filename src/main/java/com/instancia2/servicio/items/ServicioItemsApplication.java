64aa3e9e3d2638336c8e517c313c9a7a5b507a53
Gorka López-Quintana <>
2020-01-30 11:55:32 +0100

    Implementamos la gestión de errores con Hystrix con ejemplos de uso.

--- src/main/java/com/instancia2/servicio/items/ServicioItemsApplication.java
+++ src/main/java/com/instancia2/servicio/items/ServicioItemsApplication.java
@@ -2,12 +2,14 @@ package com.instancia2.servicio.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 // @RibbonClient(name = "servicio-productos") --> Si quita al tener Eureka que viene con Ribbon implicito
 @EnableFeignClients
+@EnableCircuitBreaker
 @SpringBootApplication
 public class ServicioItemsApplication {
 
--- src/main/java/com/instancia2/servicio/items/controller/ItemController.java
+++ src/main/java/com/instancia2/servicio/items/controller/ItemController.java
@@ -1,13 +1,16 @@
 package com.instancia2.servicio.items.controller;
 
 import com.instancia2.servicio.items.model.Item;
+import com.instancia2.servicio.items.model.Producto;
 import com.instancia2.servicio.items.service.IItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -23,8 +26,25 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "metodoAlternativo") // --> Metodo alternativo en caso de error al recuperar producto del servicio-productos
     @GetMapping("/listar/{id}/cantidad/{cantidad}")
     public Item findItem(@PathVariable Long id, @PathVariable Integer cantidad) {
         return itemService.findById(id, cantidad);
+    }
+
+    // Método alternativo que se ejecutará en caso de error, devolvemos un producto alternativo
+    public Item metodoAlternativo(Long id, Integer cantidad){
+        Item item = new Item();
+        Producto producto = new Producto();
+
+        producto.setId(id);
+        producto.setNombre("Producto Alternativo");
+        producto.setPrecio(500.00);
+        producto.setCreateAt(new Date());
+
+        item.setCantidad(5);
+        item.setProducto(producto);
+
+        return item;
     }
 }
