07909f7fe1016339c24a09c8db1ab0c1257f0ec2
jorgiom4 <gorkalqy@hotmail.com>
2020-01-31 14:16:43 +0100

    Implementamos pero sin llegar a funcionar RefreScope y Actuator

--- src/main/java/com/instancia2/servicio/items/controller/ItemController.java
+++ src/main/java/com/instancia2/servicio/items/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
