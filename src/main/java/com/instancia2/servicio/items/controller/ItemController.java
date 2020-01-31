0eb1a365bde00ed9cbab851b276bce63fc274cdc
jorgiom4 <gorkalqy@hotmail.com>
2020-01-31 10:52:20 +0100

    Implementamos un endpoint para recuperar los parámetros de configuración, realizando prubeas con un perfil de desarrollo y otro de producción.

--- src/main/java/com/instancia2/servicio/items/controller/ItemController.java
+++ src/main/java/com/instancia2/servicio/items/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,9 @@ import java.util.Map;
 public class ItemController {
 
     private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     // @Qualifier("serviceRestTemplate") // Para usar el servicio RestTemplate
@@ -53,6 +57,12 @@ public class ItemController {
         Map<String,String> json = new HashMap<>();
         json.put("puerto",puerto);
         json.put("texto", texto);
+
+        // Comprovamo si tenemos el perfil de desarrollo
+        if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")){
+            json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
 
         return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
     }
